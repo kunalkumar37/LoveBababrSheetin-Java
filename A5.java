85651ad259785ba8af14dfd4e645880503161e88
Kunal Kumar <61112064+kunalkumar37@users.noreply.github.com>
2022-03-27 12:41:24 +0530

    Add files via upload

---
+++ A5.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class A5 {
+
+   public static void main(String[] args) {
+       Integer []array=new Integer[]{-12, 11, -13, -5, 6, -7, 5, -3, -6};
+       Arrays.sort(array);
+       for(int i=0;i<array.length;i++){
+           System.out.println(array[i]);
+       }
+
+   } 
+}
